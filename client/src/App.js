5750026bcdf4a8cd2d1af7a71f321194f6da4ebd
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-27 20:33:11 +0300

    update resources

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import taqs_black from './drawable/taqs-black.svg';
+import taqs_white from './drawable/taqs-white.svg';
 
 
+function NavBar(){
+  return (
+    <div className="NavBar">
+      <img className="logo" src={taqs_white}/>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1>Hello World!</h1>
     </div>
   );
