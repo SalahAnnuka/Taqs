3710dd4145772e942df04466252992559e3e5b1c
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-28 13:49:58 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.listen()
+let apireq = 'https://api.openweathermap.org/data/2.5/weather?q=Amman&units=metric&appid=b5331a7dcbc34c05a7cab530bc441d05';
+
+app.get('/',function(req,res){
+    res.send('Server Start.');
+});
+
+var server = app.listen(7777,function(){
+    var port = server.address().port;
+    console.log('Server is listening at port ' + port);
+});
