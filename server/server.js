326886a0f9e470cc5a95d1a825c87342fff4f3b6
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-03-01 23:33:43 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 
-let apireq = 'https://api.openweathermap.org/data/2.5/weather?q=Amman&units=metric&appid=b5331a7dcbc34c05a7cab530bc441d05';
+
+app.get('/Result/:city', function(req,res){
+    var result = '';
+    var apireq = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&units=metric&appid=b5331a7dcbc34c05a7cab530bc441d05`;
+    console.log(apireq);
+    request(apireq, function (error, response, body) {
+        if (response.statusCode === 200)
+        {
+            result = JSON.parse(body);
+            res.json(result);
+        }
+        else {
+            console.error('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        }
+    });
+});
+
 
 app.get('/',function(req,res){
     res.send('Server Start.');
